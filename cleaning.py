9642fb6be6ef5a9117fe62913513ec70d2b1eaf6
sumanthd17 <dsumanth17@gmail.com>
2023-02-10 10:08:07 +0530

    parallelise cleaning code

--- cleaning.py
+++ cleaning.py
@@ -11,6 +11,8 @@ from indicnlp.normalize.indic_normalize import IndicNormalizerFactory
 
 from num_to_word import num_to_word
 
+from joblib import Parallel, delayed
+
 REMOVE_NUKTAS=False
 
 def normalize_sentence(sentence, lang_code):
@@ -82,7 +84,7 @@ def cleaning_pipeline(input_file, output_file, lang_code):
             ## Remove punctuations (add danda to punctuation list)
             sentence = sentence.translate(str.maketrans('', '', string.punctuation+'।'))
             ## Convert num to word (Don't do for speech transcripts! used for LM text cleaning)
-            # sentence = convert_num_to_word_sentence(sentence, lang_code
+            # sentence = convert_num_to_word_sentence(sentence, lang_code)
             ## Normalize sentence
             sentence = normalize_sentence(sentence, lang_code)
             # Drop sentence having OOD characters (Don't drop for ASR, using it to calculate stats)
@@ -105,12 +107,38 @@ def cleaning_pipeline(input_file, output_file, lang_code):
     print(f"In Dictionary sentences: {count_no_ood}/{total_sents}")
     print("Complete!")
 
+def crisp_cleaning_pipeline(inline, lang_code):
+    inline = inline.translate(str.maketrans('', '', string.punctuation+'।'))
+    inline = convert_num_to_word_sentence(inline, lang_code)
+    inline = normalize_sentence(inline, lang_code)
+    # matched_list = [char in dict_characters for char in inline]
+    # no_ood = all(matched_list)
+    # if no_ood:
+    #     return inline
+    return inline
 
 if __name__ == "__main__":
 
     input_file = sys.argv[1] 
     output_file = sys.argv[2]
     language = sys.argv[3]
-    cleaning_pipeline(input_file, output_file, language)
+
+    dict_file = f"dicts/{language}.dict.txt"
+    dict_df = pd.read_csv(dict_file, header=None, sep=' ')
+    dict_characters = ''.join(dict_df[0].to_list())
+    dict_characters += ' '
+    print("Dictionary Loaded.")
+
+    # cleaning_pipeline(input_file, output_file, language)
+    with open(input_file, "r") as f:
+        lines = f.readlines()
+    lines = [line.rstrip() for line in lines]
+    lines = [line for line in lines if line != '']
+
+    lines = Parallel(n_jobs=8)(delayed(crisp_cleaning_pipeline)(line, language) for line in tqdm(lines))
+    with open(output_file, "w") as f:
+        for line in lines:
+            if line is not None:
+                f.write(line)
 
         
